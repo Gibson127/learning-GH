4434588f12e27699c8b9a222a54fac89df5e0674
Taylor Gibson <taylorg127@gmail.com>
2021-07-28 22:30:04 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Taylor Gibson on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
